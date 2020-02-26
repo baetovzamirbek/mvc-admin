0e638ba5236db517f87e25c64dd88530ba758a17
baetovzamirbek <46441690+baetovzamirbek@users.noreply.github.com>
2020-02-26 08:16:39 +0600

    enter

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -11,7 +11,7 @@
   <link href="public/css/style.css" rel="stylesheet">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <!--connect to script -->
+  <!--connect to script   -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
